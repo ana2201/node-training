058a0c1789e4302741202e93913c0892f606d80a
ana2201 <ana.rodriguez@xmartlabs.com>
2024-01-22 14:54:08 -0300

    [add middleware] express.json

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,9 @@ const port = 3000;
 app.use(cors());
 app.use(helmet());
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use('', router);
 
 app.listen(port, () => {
--- src/middleware/message-middleware.ts
+++ src/middleware/message-middleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction  } from 'express';
 
 const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
   console.log(`${req.method} request made to ${req.path}`);
-  
+  console.log('test: express.urlencoded()',req.body);
   next();
 };
 
